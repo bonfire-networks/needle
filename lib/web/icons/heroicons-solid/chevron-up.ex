f23454e6a9e6da7021762ebc7866dea0a657b681
Mayel de Borniol <git@mayel.space>
2022-10-27 21:32:02 +1300

    clean

--- lib/web/icons/heroicons-solid/chevron-up.ex
+++
@@ -1,8 +0,0 @@
-defmodule Iconify.HeroiconsSolid.ChevronUp do
-  use Phoenix.Component
-  def render(assigns) do
-    ~H"""
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" fill-rule="evenodd" d="M14.707 12.707a1 1 0 0 1-1.414 0L10 9.414l-3.293 3.293a1 1 0 0 1-1.414-1.414l4-4a1 1 0 0 1 1.414 0l4 4a1 1 0 0 1 0 1.414Z" clip-rule="evenodd"/></svg>
-    """
-  end
-end
